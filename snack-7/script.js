8bfa756702f60f0f28106e0722c034842e03ddf7
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-02 18:37:51 +0200

    [UPD] finito stack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,14 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const idDue = students.find((student) => {
+  if (student.id === 2){
+    return true;
+  }
+  else {
+    return false;
+  }
+});
+
+console.log(idDue);
